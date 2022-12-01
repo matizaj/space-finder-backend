da2f2d918e502781877f03e5adc07a006912a2e3
Mateusz <matzaj101@gmail.com>
2022-12-01 22:38:35 +0100

    try debug

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -5,6 +5,7 @@ import {LambdaIntegration, RestApi} from 'aws-cdk-lib/aws-apigateway'
 import {join} from 'path'
 import { GenericTable } from './GenericTable';
 import {NodejsFunction} from 'aws-cdk-lib/aws-lambda-nodejs'
+import {PolicyStatement} from 'aws-cdk-lib/aws-iam';
 
 export class SpaceStack extends Stack {
     private api = new RestApi(this, 'SpaceApi');
@@ -12,18 +13,17 @@ export class SpaceStack extends Stack {
     constructor(scope: Construct, id: string, props: StackProps){
         super(scope, id, props )
 
-        const helloLambda= new LambdaFunction(this, 'helloLambda', {
-            runtime: Runtime.NODEJS_14_X,
-            code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
-            handler: 'hello.main'
-        })
-
         const helloLambdaNodejs = new NodejsFunction(this, 'helloLambdaNodejs', {
             entry: join(__dirname, '..', 'services', 'node-lambda', 'hello.ts'),
             handler: 'handler'
         })
 
-        const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+        const s3ListPolicy = new PolicyStatement();
+        s3ListPolicy.addActions('s3:ListAllMyBuckets');
+        s3ListPolicy.addResources('*');
+        helloLambdaNodejs.addToRolePolicy(s3ListPolicy);
+
+        const helloLambdaIntegration = new LambdaIntegration(helloLambdaNodejs);
         const helloLambdaResource = this.api.root.addResource('hello');
         helloLambdaResource.addMethod('GET', helloLambdaIntegration);
     }
--- services/node-lambda/hello.ts
+++ services/node-lambda/hello.ts
@@ -1,9 +1,14 @@
-import {v4} from 'uuid';
+import {S3} from 'aws-sdk';
+
+const s3Client = new S3();
 
 async function handler(event:any, context: any) {
+    const baukets = await s3Client.listBuckets().promise();
+    console.log('receive a event');
+    console.log(event)
     return {
         statusCode:200,
-        body: 'hello form ts lambda' + v4()
+        body: 'here are your buckets: ' + JSON.stringify(baukets.Buckets)
     }
 }
 
