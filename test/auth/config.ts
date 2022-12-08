8b6b88fcd22fa99fc28a9447b61fc0f799107510
Mateusz Zajac <35918401+matizaj@users.noreply.github.com>
2022-12-08 10:54:37 +0100

    Delete config.ts

--- test/auth/config.ts
+++
@@ -1,8 +0,0 @@
-export const config = {
-    region: 'us-east-1',
-    userPoolId: 'us-east-1_RjXiize5b',
-    appClientId: '6edh6jugo2ir6svfchtli39jjn',
-    testUserName: 'mateusz2',
-    testUserPassword: 'QWEasd123!',
-    identityPoolId: 'us-east-1:5f22e9d4-c05f-41a1-b79e-30e24b992c6d'
-}
